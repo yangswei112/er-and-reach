73f09ab5388cddd54e6bc61735eec3842cfb1fa5
Yayang NAAW <yayang1123@gmail.com>
2024-01-09 15:13:29 +0700

    create usernames.txt file and use it to store all usernames

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from instagram import Instagram
 from tiktok import TikTok
 
-uname = ["yangswei", "selenagomez", "taylorswift"]
+# load file contains username
+with open("usernames.txt") as file:
+    uname = file.read().split("\n")
 
+# find IG ER
 ig = Instagram(username=uname)
 er_ig = ig.get_er()
 print(er_ig)
 
+# find Tiktok ER & Reach
 tt = TikTok(username=uname)
 er_reach_tt = tt.get_er_reach()
 er_tt = er_reach_tt[0]
