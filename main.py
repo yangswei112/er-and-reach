59a097c78532e840dac6efbe3f2221aa7fdce759
Yayang NAAW <yayang1123@gmail.com>
2024-01-10 11:38:06 +0700

    store results to csv

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from instagram import Instagram
 from tiktok import TikTok
+from helperfunction import convert_amount
+import pandas as pd
 
 # load file contains link profile
 with open("link_profile.txt") as file:
@@ -16,6 +18,9 @@ with open("link_profile.txt") as file:
 # find IG ER
 ig = Instagram(username=uname)
 er_ig = ig.get_er()
+er_ig_results = pd.DataFrame({"Username": uname,
+                              "ER": [str(er)+'%' for er in er_ig.values()]})
+er_ig_results.to_csv("results.csv", index=False)
 print(er_ig)
 
 # find Tiktok ER & Reach
@@ -23,5 +28,9 @@ tt = TikTok(username=uname)
 er_reach_tt = tt.get_er_reach()
 er_tt = er_reach_tt[0]
 reach_tt = er_reach_tt[1]
+er_reach_tt_results = pd.DataFrame({"Username": uname,
+                                    "ER": [str(er)+'%' for er in er_tt.values()],
+                                    'Reach': [convert_amount(reach) for reach in reach_tt.values()]})
+er_reach_tt_results.to_csv("results.csv", index=False)
 print(er_tt)
 print(reach_tt)
