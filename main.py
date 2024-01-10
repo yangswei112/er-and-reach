98f1f3435fb0e151eec2c87277dcff3c326d021e
Yayang NAAW <yayang1123@gmail.com>
2024-01-10 11:45:18 +0700

    create a conditional whether it's tiktok or instagram account

--- main.py
+++ main.py
@@ -15,22 +15,24 @@ with open("link_profile.txt") as file:
             "?")[0].strip("/")
         for link in links]
 
-# find IG ER
-ig = Instagram(username=uname)
-er_ig = ig.get_er()
-er_ig_results = pd.DataFrame({"Username": uname,
-                              "ER": [str(er)+'%' for er in er_ig.values()]})
-er_ig_results.to_csv("results.csv", index=False)
-print(er_ig)
+# Find ER & Reach on Instagram
+if "instagram" in links[0]:
+    ig = Instagram(username=uname)
+    er_ig = ig.get_er()
+    er_ig_results = pd.DataFrame({"Username": uname,
+                                  "ER": [str(er)+'%' for er in er_ig.values()]})
+    er_ig_results.to_csv("results.csv", index=False)
+    print(er_ig)
 
-# find Tiktok ER & Reach
-tt = TikTok(username=uname)
-er_reach_tt = tt.get_er_reach()
-er_tt = er_reach_tt[0]
-reach_tt = er_reach_tt[1]
-er_reach_tt_results = pd.DataFrame({"Username": uname,
-                                    "ER": [str(er)+'%' for er in er_tt.values()],
-                                    'Reach': [convert_amount(reach) for reach in reach_tt.values()]})
-er_reach_tt_results.to_csv("results.csv", index=False)
-print(er_tt)
-print(reach_tt)
+# Find ER & Reach on Tiktok
+elif "tiktok" in links[0]:
+    tt = TikTok(username=uname)
+    er_reach_tt = tt.get_er_reach()
+    er_tt = er_reach_tt[0]
+    reach_tt = er_reach_tt[1]
+    er_reach_tt_results = pd.DataFrame({"Username": uname,
+                                        "ER": [str(er)+'%' for er in er_tt.values()],
+                                        'Reach': [convert_amount(reach) for reach in reach_tt.values()]})
+    er_reach_tt_results.to_csv("results.csv", index=False)
+    print(er_tt)
+    print(reach_tt)
